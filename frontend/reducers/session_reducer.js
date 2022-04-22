b8c5b6a18ed761048d43d8c469f39b4fdc5babaf
App Academy Student <email@email.com>
2022-04-22 15:27:20 -0400

    sessionReducer complete

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,22 @@
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER
+} from '../actions/session_actions';
+
+const _nullState = {
+  currUserId: null
+};
+
+const sessionReducer = (state, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return Object.assign({}, {currUserId: action.currUser.id});
+    case LOGOUT_CURRENT_USER:
+      return _nullState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
