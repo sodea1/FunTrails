3405bf3a49563dba81a20b8a1f34b35c30d2177c
seanodea <seanodea11@gmail.com>
2022-05-19 18:40:44 -0400

    move require_master_key above active_storage in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :amazon_prod
   config.require_master_key = true
+  config.active_storage.service = :amazon_prod
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
